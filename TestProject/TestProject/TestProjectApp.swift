ef142629bb9b1361506f8e78063a7b6249c237f7
kiana1207423 <your_email@example.com>
2024-06-18 18:04:03 +0800

    1

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by 3033 on 2024/6/18.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
